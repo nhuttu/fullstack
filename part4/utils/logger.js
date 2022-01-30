7dd29c571d13a910e9778158a199a5a3a39740e9
nhuttu <nikohuttu1@gmail.com>
2022-01-30 13:06:46 +0200

    commit

--- part4/utils/logger.js
+++ part4/utils/logger.js
@@ -1,10 +1,12 @@
 const info = (...params) => {
+    // eslint-disable-next-line no-undef
     if (process.env.NODE_ENV !== 'test') {
         console.log(...params)
     }
 }
 
 const error = (...params) => {
+    // eslint-disable-next-line no-undef
     if (process.env.NODE_ENV !== 'test') {
         console.error(...params)
     }
