999220d4c05f057cf03358bf8b4586aee1dc43d3
nhuttu <nikohuttu1@gmail.com>
2022-01-30 10:19:26 +0200

    commit

--- part4/models/user.js
+++ part4/models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
     blogs: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Blog'
     }],
+    username: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     passwordHash: String,
 
