e290e1f7f94447a993e0918baee0eb2c2a098e52
nhuttu <nikohuttu1@gmail.com>
2022-03-13 15:05:47 +0200

    commit

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
 export const useField = (type) => {
-    const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
-    const onChange = e => {
-        setValue(e.target.value)
-    }
-    const reset = () => {
-        setValue('')
-    }
-    return {
-        reset,
-        type,
-        value,
-        onChange,
-    }
-}
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  const reset = () => {
+    setValue("");
+  };
+  return {
+    reset,
+    type,
+    value,
+    onChange,
+  };
+};
